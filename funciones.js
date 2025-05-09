f15108f7056dc264a7de551d4c26d2bd31d4cb04
abrunaal <bana14470@gmail.com>
2025-05-08 22:38:46 -0300

    first commit

---
+++ funciones.js
@@ -0,0 +1,21 @@
+
+let usuarios = [];
+const agregar_usuario = (nombre,apellido,cedula,telefono)=>{
+    if(nombre && apellido && cedula){
+    let usuario = {
+        nombre:nombre, //propiedades de la funcion con su valor asignado, se asignan por posiciones, por lo que se deben agregar en orden
+        apellido:apellido, 
+        cedula:cedula,
+        telefono:telefono
+    }
+    usuarios.push(usuario)
+    console.log(usuarios)
+    }
+    else{
+        console.log("debe ingresar nombre, apellido y cedula");
+}
+}
+
+export{
+    agregar_usuario
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const { agregar_usuario } = require("./funciones.js")
+
+agregar_usuario("Ana","Vieyto",4545450,"09876444")
